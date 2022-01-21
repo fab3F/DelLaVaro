7decc124002da0e3ef788589ba5516752c6f6967
fab3F <fbnhaukede@gmail.com>
2022-01-21 23:06:35 +0100

    Changes

---
+++ src/main/java/fab3f/dellavaro/listeners/BlockListener.java
@@ -0,0 +1,6 @@
+package fab3f.dellavaro.listeners;
+
+import org.bukkit.event.Listener;
+
+public class BlockListener implements Listener {
+}
---
+++ src/main/java/fab3f/dellavaro/listeners/ConnectionListener.java
@@ -0,0 +1,7 @@
+package fab3f.dellavaro.listeners;
+
+import org.bukkit.event.Listener;
+
+public class ConnectionListener implements Listener {
+
+}
