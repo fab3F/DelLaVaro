d21988148fa176c5d156289d4fb2a942d4ea624c
fab3F <fbnhaukede@gmail.com>
2022-01-21 20:05:30 +0100

    Initial commit

---
+++ src/main/java/fab3f/dellavaro/DelLaVaro.java
@@ -0,0 +1,17 @@
+package fab3f.dellavaro;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DelLaVaro extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
